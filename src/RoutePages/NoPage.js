90a258726879be6d6175859e62ff53c1005fbe91
Milton Khan <43887779+srmiltonkhan@users.noreply.github.com>
2022-02-01 12:32:19 +0600

    first commit1

---
+++ src/RoutePages/NoPage.js
@@ -0,0 +1,5 @@
+const NoPage = () => {
+  return <h1 style={{ backgroundColor: "blue", color: "red" }}>404</h1>;
+};
+
+export default NoPage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Layout from "./RoutePages/Layout";
+import Home from "./RoutePages/Home";
+import Blogs from "./RoutePages/Blogs";
+import Contact from "./RoutePages/Contact";
+import NoPage from "./RoutePages/NoPage";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./index.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogst" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
